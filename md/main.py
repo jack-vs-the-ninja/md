6a4ed4fdcf125e1126647b88001da8800cfd7712
jack-vs-the-ninja <60750486+jack-vs-the-ninja@users.noreply.github.com>
2021-02-22 19:58:00 -0800

    gift-wrapped (#1)

--- md/main.py
+++ md/main.py
@@ -1,6 +1,7 @@
 import os
 from contextlib import contextmanager
 from pathlib import Path
+import shutil
 from tempfile import NamedTemporaryFile
 
 import yaml
@@ -11,7 +12,6 @@ ns = Collection()
 
 @contextmanager
 def override_config_file(config_file, **kwargs):
-    # TODO should use MKDOCS_PROJECT_ENV in the load stack. Reminder: 1. cli args 2. environ 3. yml merge
 
     with open(config_file) as base:
         dd = yaml.load(base, Loader=yaml.FullLoader)
@@ -22,8 +22,18 @@ def override_config_file(config_file, **kwargs):
         yield fp.name
 
 
-@task(name='serve', default=True)
+@task(
+    name='serve',
+    default=True,
+    help={
+        'docs-dir': 'path to directory containing markdown files',
+        'dev-addr': '0.0.0.0:8000'
+    },
+)
 def mkdocs_serve(ctx, docs_dir=None, dev_addr=None):
+    """
+    Serve some local markdown.
+    """
 
     if docs_dir is None:
         docs_dir = '.'
@@ -31,17 +41,30 @@ def mkdocs_serve(ctx, docs_dir=None, dev_addr=None):
     docs_dir_path = Path(docs_dir).absolute()
 
     if dev_addr is None:
-        dev_addr = 'localhost:8000'
+        dev_addr = '0.0.0.0:8000'
 
-    assert os.environ.get('MKDOCS_PROJECT_DIR') and os.environ.get('MKDOCS_PROJECT_ENV'),\
-        'please set MKDOCS_PROJECT_DIR and MKDOCS_PROJECT_ENV'
+    os.environ.setdefault('MKDOCS_PROJECT_DIR', str(Path(__file__).parent / 'mkdocs-example-tree'))
+    os.environ.setdefault('MKDOCS_PROJECT_ENV', 'default')
 
-    config_file_basename = os.environ.get('MKDOCS_PROJECT_CONFIG_FILE_BASENAME') or 'mkdocs.yml'
-    config_file = Path(os.environ['MKDOCS_PROJECT_DIR']) / config_file_basename
-    assert config_file.exists(), f'couldn\'t find {config_file}. Create it or set MKDOCS_PROJECT_CONFIG_FILE_BASENAME'
+    pp = Path(os.environ['MKDOCS_PROJECT_DIR'])
+    unlink = []
+    for ea in (
+        pp / 'docs/css',
+        pp / 'docs/js',
+    ):
+        link_name = docs_dir_path / ea.name
+        link_name.unlink(missing_ok=True)
+        link_name.symlink_to(ea)
+        unlink.append(link_name)
+
+    config_file = pp / 'mkdocs.yml'
+    assert config_file.exists(), f'couldn\'t find {config_file}.'
 
     with override_config_file(config_file, docs_dir=str(docs_dir_path)) as tmp_config_file:
         ctx.run(f'mkdocs serve --dev-addr {dev_addr} --config-file {tmp_config_file}')
+
+    for ea in unlink:
+        ea.unlink(missing_ok=True)
 
 
 ns.add_task(mkdocs_serve)
--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+
+def glob_fix(package_name, glob):
+    # this assumes setup.py lives in the folder that contains the package
+    package_path = Path(f'./{package_name}').resolve()
+    return [str(path.relative_to(package_path)) for path in package_path.glob(glob)]
+
 
 setup(
     name='md',
     version='0.1.0',
     packages=['md'],
-    install_requires=['invoke', 'mkdocs-material'],
+    install_requires=['invoke', 'mkdocs-material', 'mkdocs-exclude'],
     entry_points={
         'console_scripts': ['md = md.main:program.run']
-    }
+    },
+    package_data={'md': [*glob_fix('md', 'mkdocs-example-tree/**/*')]},
 )
